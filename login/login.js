fb7dde170dbcc442aba36fd9e3a2c84ca7032797
PC <PC@gmail.com>
2024-07-02 22:04:43 +0700

    check input from user in js

--- login/login.js
+++ login/login.js
@@ -1,33 +1,36 @@
-var loginApp = angular.module('loginApp' , []);
-loginApp.controller('loginCtrl' , function($scope , $http){
+var loginApp = angular.module('loginApp', []);
+loginApp.controller('loginCtrl', function ($scope, $http) {
 
+
+    $scope.user = {};
     $scope.errorMessage = '';
     $scope.successMessage = '';
-    $scope.user = {};
-  
 
-    $scope.loginSubmit = function(){
 
-         var regex = /^[a-zA-Z0-9]+$/;
-         if (!regex.test($scope.user.username) || !regex.test($scope.user.password)) {
+    $scope.loginSubmit = function () {
+
+        var regex = /^[a-zA-Z0-9]+$/;
+        if ($scope.user.username == undefined) {
+            $scope.errorMessage = 'Username or password undefiend.';
+            return;
+        }
+        if (!regex.test($scope.user.username) || !regex.test($scope.user.password)) {
             $scope.errorMessage = 'Username and password must contain only letters (a-z) and numbers (0-9).';
             return;
-        } if($scope.user.username.trim() === '' || $scope.user.password.trim() === '') {
-            $scope.errorMessage = 'Username and password don`t need space';
-            return
         }
-        
+
         $http({
-            method:'post',
-            url:'login.php',
-            Headers : '',
-            data:$scope.user
-        }).then(function successCallback(response){
+            method: 'post',
+            url: 'login.php',
+            Headers: '',
+            data: $scope.user
+        }).then(function successCallback(response) {
             $scope.successMessage = response.data.success;
             $scope.errorMessage = response.data.faild;
-          
-           
-        } , function errorCallback(response){});
+
+        }, function errorCallback(response) {
+            console.log(response)
+        });
 
     }
 
--- register/register.js
+++ register/register.js
@@ -1,37 +1,47 @@
-var registerApp = angular.module('registerApp' , []);
-registerApp.controller('registerCtrl' , function($scope , $http){
-   
+var registerApp = angular.module('registerApp', []);
+registerApp.controller('registerCtrl', function ($scope, $http) {
+
 
     $scope.user = {}
 
-    $scope.registerSubmit = function(){
+    $scope.registerSubmit = function () {
 
         $scope.errorMessage = '';
         $scope.successMessage = '';
-        
-         var regex = /^[a-zA-Z0-9]+$/;
-         if (!regex.test($scope.user.username) || !regex.test($scope.user.password)) {
-            $scope.errorMessage = 'Username and password must contain only letters (a-z) and numbers (0-9).';
-            return;
-        } if ($scope.user.username.length < 5 || $scope.user.password.length < 5) {
-            $scope.errorMessage = 'Username and password must be at least 5 characters long.';
-            return
-        } if($scope.user.username.trim() === '' || $scope.user.password.trim() === '') {
-            $scope.errorMessage = 'Username and password don`t need space';
-            return
-        }
-        
 
-        $http({
-            method:'post',
-            url:'register.php',
-            Headers : '',
-            data:$scope.user
-        }).then(function successCallback(response){
-            $scope.successMessage = response.data.data
+        var regex = /^[a-zA-Z0-9]+$/;
+
+        // Check null
+        if ($scope.user.username == undefined || $scope.user.password == null){
+            $scope.errorMessage = 'Username or password undefiend.';
+            console.log($scope.user.password)
+            return;
+        }
+        // check char > 5
+        if ($scope.user.username.length < 5 || $scope.user.password.length < 5) {
+            $scope.errorMessage = 'Username and password must be at least 5 characters long.';
+            return;
+        }
+        // check special letters
+        if (!regex.test($scope.user.username) || !regex.test($scope.user.password)) {
+            $scope.errorMessage = 'Username and password must contain only letters (a-z) and numbers (0-9).';
+            console.log($scope.user.password)
+
+            return;
+        }
       
 
-        } , function errorCallback(response){
+        $http({
+            method: 'post',
+            url: 'register.php',
+            Headers: '',
+            data: $scope.user
+        }).then(function successCallback(response) {
+            $scope.successMessage = response.data.data;
+
+
+        }, function errorCallback(response) {
+            console.log(response);
 
         });
 
