1848a1dd1403d25141ca386703798ac5f512a883
Macbook <taesaksit@gmail.com>
2024-07-05 20:57:21 +0700

    stable

--- condb.php
+++ condb.php
@@ -2,7 +2,7 @@
 
 $dbhost = "localhost";
 $dbuser = "root";
-$dbpass = "";
+$dbpass = "root";
 $dbname = "authen";
 $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 
--- pages/destroy.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_destroy();
-
-header('location: ../login-register/indexlogin.php')
-
-?>
